108bca724b5d5bd55e95eef3533c888cbf7fc72f
Namaste Reid <namaste.reid@gmail.com>
2019-01-12 18:12:08 -0700

    start maximized, close with escape

--- main.cpp
+++ main.cpp
@@ -10,27 +10,33 @@ int main() {
     }
     SDL_Window *window = nullptr;
     SDL_Renderer *renderer = nullptr;
-
-    if (SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-        return 3;
-    }
-
+    SDL_CreateWindowAndRenderer(1024, 768, SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED, &window, &renderer);
     SDL_SetRenderDrawColor(renderer, 0, 200, 200, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
     SDL_Event event;
 
+    bool loop = true;
     do
     {
-//        std::cout << "event type: " << event.type << std::endl;
-        int type = event.type;
-        if(type == SDL_KEYDOWN || type == SDL_QUIT) {
-            break;
+        std::cout << "event type: " << event.type << std::endl;
+        switch(event.type) {
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        loop = false;
+                        break;
+                    default:break;
+                }
+                break;
+            case SDL_QUIT:
+                loop = false;
+                break;
+            default:break;
         }
     }
-    while(SDL_WaitEvent(&event));
+    while(SDL_WaitEvent(&event) && loop);
 
     SDL_Quit();
     return 0;
